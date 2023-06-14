4f54804620fcb6e4f7692b453dcd8e90d58cdec5
tomaslami <101568473+tomaslami@users.noreply.github.com>
2023-06-14 09:47:54 -0300

    news

---
+++ prueba-proyecto/src/components/InputVariants.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+interface InputProps{
+    
+}
+
+const InputVariants = () => {
+
+    return (
+        <div className='relative'>
+            <input type="text" />
+        </div>
+    )
+}
+
+export default InputVariants
