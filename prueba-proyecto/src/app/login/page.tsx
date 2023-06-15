21ad9369e4a9743977de2bed6d31dc672629cb36
tomaslami <tomaslamiguralnik@gmail.com>
2023-06-14 21:09:16 -0300

    Nuevos commits, login y register

--- prueba-proyecto/src/app/login/page.tsx
+++ prueba-proyecto/src/app/login/page.tsx
@@ -1,10 +1,68 @@
-import Image from 'next/image'
+'use client';
+
+import InputVariants from '@/components/InputVariants'
+import { useState } from 'react'
 
 export default function Login() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [variant, setVariant] = useState('register')
+
+  const handleClick = () => {
+    if (variant === 'login') {
+      setVariant('register')
+    } else {
+      setVariant('login')
+    }
+  }
+
   return (
     <>
-    
-    
+      <div className="w-full h-screen bg-indigo-200 flex justify-center items-start ">
+        <div className="w-96 h-[28rem] rounded-lg lg:2/5 lg:w-max-md self-center px-12 py-12 bg-blue-500 flex flex-col gap-4">
+          <h2 className="text-white h-1/5 text-4xl font-bold flex justify-center">
+            {variant === 'login' ? 'Inicia Sesión' : 'Registro'}
+          </h2>
+          <div className="w-full h-4/5 flex flex-col gap-4 justify-start items-center">
+            <InputVariants
+              label='Username'
+              onChange={(e: any) => setName(e.target.value)}
+              id='name'
+              type='text'
+              value={name}
+            />
+            {variant === 'register' && (
+              <InputVariants
+                label='Email'
+                onChange={(e: any) => setEmail(e.target.value)}
+                id='email'
+                type='text'
+                value={email}
+              />
+            )
+            }
+            <InputVariants
+              label='Password'
+              onChange={(e: any) => setPassword(e.target.value)}
+              id='password'
+              type='password'
+              value={password}
+            />
+            <button className='w-40 h-12 rounded-md bg-green-500 hover:bg-green-400 transition font-bold'>
+              {variant === 'login' ? 'Iniciar Sesión' : 'Registrarse'}
+            </button>
+            <p className="text-white flex flex-row gap-2 w-full">
+              {variant === 'login' ? 'No te registraste todavía??' : 'Ya tenes una cuenta?'}
+              <span className='text-white font-bold hover:underline cursor-pointer' onClick={handleClick}>
+                {variant === 'login' ? 'Registrarse' : 'Inicia Sesión'}
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+
     </>
   )
 }
---
+++ prueba-proyecto/src/components/InputVariants.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+
+interface InputProps {
+  id: string,
+  onChange: any,
+  value: string,
+  label: string,
+  type: string
+}
+
+const InputVariants: React.FC<InputProps> = ({
+  id,
+  onChange,
+  type,
+  label,
+  value
+}) => {
+
+  return (
+    <div className='relative'>
+      <input
+        onChange={onChange}
+        type="text"
+        id={id}
+        className='
+                block 
+                px-6
+                pt-6
+                pb-1
+                rounded-md
+                w-full
+                text-md
+                text-black
+                font-bold
+                apparence-none
+                focus: outline-none
+                focus: ring-0
+                peer'
+        placeholder=' '
+        autoComplete='off'
+      />
+      <label
+        className='
+                absolute
+                text-md
+                text-black
+                duration-150
+                transform
+                -translate-y-3
+                scale-75
+                top-4
+                z-10
+                origin-[0]
+                left-6
+                peer-placeholder-shown:scale-100
+                peer-placeholder-shown:translate-y-0
+                peer-focus:scale-75
+                peer-focus:-translate-y-3
+                cursor-pointer
+
+                '
+        htmlFor={id}>
+        {label}
+      </label>
+    </div>
+  )
+}
+
+export default InputVariants
