0f4029340eb32b63f96437d864c04f62edae1995
tomaslami <tomaslamiguralnik@gmail.com>
2023-07-06 21:25:19 -0300

    swiper changes

--- prueba-proyecto/src/components/Carousel.tsx
+++ prueba-proyecto/src/components/Carousel.tsx
@@ -1,72 +1,75 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import { EffectCoverflow, Pagination, Navigation } from 'swiper';
-import { CldImage } from 'next-cloudinary';
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import dir_url from '@/lib/url';
-
+import React, { useState } from 'react'
+import { Swiper, SwiperSlide } from "swiper/react"
+import "swiper/css"
+import "swiper/css/effect-coverflow"
+import "swiper/css/pagination"
+import { EffectCoverflow, Pagination, Autoplay } from "swiper"
+import { identifierToKeywordKind } from "typescript"
+import Image from "next/image"
+import Foto1 from '../../public/assets/Foto1.png'
+import Foto2 from '../../public/assets/Foto2.png'
+import Foto3 from '../../public/assets/Foto3.png'
+import { MuseosProps } from '@/types'
 
 const Carousel = () => {
-  const [museos, setMuseos] = useState([]);
+  const [museos, setMuseos] = useState<MuseosProps[]>([])
 
-  useEffect(() => {
-    viewMuseos();
-  }, []);
-
-  const viewMuseos = async () => {
-    try {
-      const response = await axios.get(`${dir_url}/api/museos`);
-      setMuseos(response.data);
-    } catch (error) {
-      console.error(error);
+  const dataCarousel = [
+    {
+      id: 1,
+      title: 'Museo de Arte Moderno',
+      Image: Foto1
+    },
+    {
+      id: 2,
+      title: 'Museo de Arte Contemporáneo',
+      Image: Foto2
+    },
+    {
+      id: 3,
+      title: 'Museo de Arte Antiguo',
+      Image: Foto3
+    },
+    {
+      id: 4,
+      title: 'HolaHolaHola',
     }
-  };
-
+  ]
 
 
   return (
-    <div className="w-full h-[600px] flex justify-center items-center flex-row gap-4">
-      <Swiper
-        effect="coverflow"
-        grabCursor={true}
-        centeredSlides={true}
-        loop={true}
-        slidesPerView={'auto'}
-        coverflowEffect={{
-          rotate: 0,
-          stretch: 0,
-          depth: 1000,
-          modifier: 2.5,
-        }}
-        pagination={{ clickable: true }}
-        navigation={{
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-
-        }}
-        modules={[EffectCoverflow, Pagination, Navigation]}
-        className="my-custom-carousel"
-      >
-        {museos.map(({ id, name }) => (
-          <SwiperSlide key={id}>
-            <div className="carousel-item">
-              <CldImage
-                className='carousel-image'
-                width='200'
-                height='200'
-                src={id}
-                alt="Imagen Museo" />
-              <h1 className="">{name}</h1>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+    <div className='w-full h-screen flex justify-center items-center'>
+      <div className='w-96 h-56 flex justify-center items-center'>
+        <Swiper
+          effect={"coverflow"}
+          centeredSlides={true}
+          slidesPerView={1.5}
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+          coverflowEffect={{
+            rotate: 0,
+            stretch: 0,
+            depth: 700,
+            modifier: 1,
+          }}
+          loop={true}
+          modules={[Autoplay, EffectCoverflow]}
+          className="mySwiper p-12"
+        >
+          {dataCarousel.map(({ id, title, Image }) => {
+            return (
+              <SwiperSlide key={id}>
+                <div className='w-64 h-40 bg-red flex justify-center items-center'>
+                  <h1>{title}</h1>
+                </div>
+              </SwiperSlide>
+            )
+          })}
+        </Swiper>
+      </div>
     </div>
-  );
-};
-
-export default Carousel;
+  )
+}
+export default Carousel
