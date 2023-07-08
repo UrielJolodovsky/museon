30fcaba99ef6b63ba6c2a50d280fe7dcb66090c2
Uriel Jolodosvky <46960341@est.ort.edu.ar>
2023-07-08 12:33:15 -0300

    minor changes

--- prueba-proyecto/src/app/dashboard/museos/components/Colecciones.tsx
+++ prueba-proyecto/src/app/dashboard/museos/components/Colecciones.tsx
@@ -23,19 +23,13 @@ const Museos = () => {
       })
   }
 
-  const sendemail = async () => {
-    await axios.get(`${dir_url}/api/verification_email`).then((response) => {
-      console.log(response.data)
-    })
-  }
-
 
   return (
     <div className='w-full flex flex-row gap-4'>
       {museos.map(({ id, name }) => {
         return (
           <div className=' w-full mr-10 ml-10 flex flex-col justify-center items-center gap-6' key={id}>
-            <CldImage className='rounded-xl flex flex-row' width='300' height='300' src={id} alt="Imagen Museo" />
+            <CldImage className='rounded-xl flex flex-row' width={300} height={300} src={id} alt="Imagen Museo" />
             <h1>{name}</h1>
             <button onClick={() => router.push(`/dashboard/${id}`)} className='w-full h-12 bg-btnForm text-white font-bold text-lg rounded-xl hover:bg-btnFormHover transition' >
               Ir al museo
