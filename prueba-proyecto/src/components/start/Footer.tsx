202dcab233d84617318483ba81e109b21880fef3
tomaslami <101568838+tomaslami@users.noreply.github.com>
2023-07-11 17:26:39 -0300

    fixed swiper and organitation

---
+++ prueba-proyecto/src/components/start/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='w-full h-[2 00px] bg-dashHover flex justify-center items-center'>
+     </div>   
+  )
+}
+
+export default Footer
