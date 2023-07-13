710c865af9451d83f72a2ec289786afa1b19c5f2
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2023-07-13 15:08:36 -0300

    SearchBar for museums added

--- prueba-proyecto/src/app/dashboard/museos/components/Colecciones.tsx
+++ prueba-proyecto/src/app/dashboard/museos/components/Colecciones.tsx
@@ -11,6 +11,18 @@ const Museos = () => {
   const [museos, setMuseos] = useState<MuseosProps[]>([])
   const router = useRouter()
   const filtro = ['Deporte', 'Arte', 'Historia', 'Galerias']
+  const [filtered, setFiltered] = useState<MuseosProps[]>([]);
+
+  const FilterMuseums = (search: String) => {
+    if (search === '') {
+      setFiltered(museos)
+    }
+    else {
+      setFiltered(museos.filter((museo) => {
+        return museo.name.toLowerCase().includes(search.toLowerCase())
+      }))
+    }
+  }
 
   useEffect(() => {
     viewMuseos()
@@ -34,6 +46,15 @@ const Museos = () => {
             <button onClick={() => router.push(`/dashboard/${id}`)} className='w-full h-12 bg-btnForm text-white font-bold text-lg rounded-xl hover:bg-btnFormHover transition' >
               Ir al museo
             </button>
+            <input type='text' placeholder='search' onChange={(ev) => FilterMuseums(ev.target.value)} />
+            {filtered.length > 0 ? filtered.map(({id, name}) => {
+              return (
+                <div key={id}>
+                  <h1>{name}</h1>
+                </div>
+              )
+            }) : <h1>There is no match</h1>
+            }
           </div>
         )
       })
