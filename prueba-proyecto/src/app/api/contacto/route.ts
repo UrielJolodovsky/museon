4e9dab9e350873e3c17ccc09d493f855834ec276
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2023-08-07 09:53:01 -0300

    Backend from Contacto done and Message is sent

---
+++ prueba-proyecto/src/app/api/contacto/route.ts
@@ -0,0 +1,28 @@
+import { emailPayload, transporter } from "@/lib/config/nodemailer";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { authOptions } from "../auth/[...nextauth]/route";
+
+export async function POST(req: NextRequest, res: NextResponse) {
+    
+    const { message, user } = await req.json()
+    const session = await getServerSession(authOptions)
+
+
+    if (message.length === 0) {
+        return new NextResponse("Your message is empty", {status: 400})
+    }
+
+    else if (session?.user.name !== user || user === undefined) {
+        return new NextResponse("You are probably not logged in", {status:400})
+    }
+
+    const info = await transporter.sendMail({
+        ...emailPayload('museon.proyecto@gmail.com'),
+        subject: 'Contacto de usuario',
+        text: 'Prueba',
+        html: `<h1>Message from: ${user}</h1> <h2>Content: ${message} </h2>`
+    })
+
+    return new NextResponse("Message sent successfully", {status: 200})
+}
--- prueba-proyecto/src/contacto/Contacto.tsx
+++ prueba-proyecto/src/contacto/Contacto.tsx
@@ -1,12 +1,43 @@
-import React from 'react'
+import dir_url from '@/lib/url'
+import axios from 'axios'
+import { useSession } from 'next-auth/react'
+import React, { ChangeEvent, useState } from 'react'
+import { toast } from 'react-hot-toast'
 
 const contacto = () => {
+
+const {data: sessionData} = useSession()
+  const [message, setMessage] = useState('')
+  const SendForm = async () => {
+    if (sessionData?.user.id === undefined) {
+      toast.error("You are not logged in")
+    }
+    else if (message.length === 0) {
+      toast.error("Your message is empty")
+    }
+    else {
+      try {
+      axios.post(`${dir_url}/api/contacto`, {
+        message: message,
+        user: sessionData.user.name
+      }).then((response) => {
+        toast.success(response.data)
+      }).catch((err) => {
+        toast.error(err.response.data)
+      })
+    } catch(error) {
+      toast.error("Something went wrong")
+    }
+    }
+  }
+
+
   return (
     <div className='flex w-full h-screen justify-center items-center'>
       <div className='flex w-2/3 h-2/3 justify-start items-center flex-col gap-10 '>
       <h1 className='text-3xl font-bold'>Comunicate con nuestro equipo</h1>
-      <form action="" className='w-1/2 h-full flex justify-center items-center  bg-black p-10'>
-        <textarea autoComplete='off' name='message' id='message' placeholder='Mensaje' className='w-2/3 h-2/3 p-3 bg-transparent text-white border-white border-2 text-left pt-3 resize-none'></textarea>
+      <form onSubmit={SendForm} action="" className='w-1/2 h-full flex justify-center items-center  bg-black p-10'>
+        <textarea onChange={(ev: ChangeEvent<HTMLTextAreaElement>) => setMessage(ev.target.value)} autoComplete='off' name='message' id='message' placeholder='Mensaje' className='w-2/3 h-2/3 p-3 bg-transparent text-white border-white border-2 text-left pt-3 resize-none'></textarea>
       </form>
       </div>
     </div>
