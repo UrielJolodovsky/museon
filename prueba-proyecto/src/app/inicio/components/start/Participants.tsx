e7534a53fc7d9074dd356807b997dec261dc9942
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2023-10-05 11:31:49 -0300

    fix Landing Page

--- prueba-proyecto/src/app/inicio/components/start/Participants.tsx
+++ prueba-proyecto/src/app/inicio/components/start/Participants.tsx
@@ -5,15 +5,15 @@ import { CldImage } from 'next-cloudinary'
 
 const Participants = () => {
   return (
-    <section className='w-[300px] h-[400px] flex justify-center items-center flex-row gap-1 container-participants '>
-      <div className=' h-full lg:h-1/2 lg:w-full flex justify-center items-center'>
-        <div className='w-full h-1/2 flex justify-end items-end md:order-2 md:flex md:justify-center md:items-center'>
-          <CldImage src='PiramidIcon' width={0} height={0} alt={"Piramid"} className='w-[600px] h-[500px]' />
+    <section className='w-2/3 h-[200px] flex justify-start items-center flex-col md:flex-row gap-2 pt-5'>
+      <div className='w-full h-1/2 md:w-1/2 md:h-full flex justify-center items-center order-2 md:order-1'>
+        <div className='w-full h-full flex justify-center items-center'>
+          <CldImage src='PiramidIcon' alt='PiramidImage' width={0} height={0} className='w-[300px] h-[200px]' ></CldImage>
         </div>
       </div>
-      <div className='w-1/2 flex justify-center items-center flex-col gap-2 part-texts '>
-        <h2 className='w-full font-extrabold text-center text-4xl md:text-2xl'>¿Quienes participan?</h2>
-        <h4 className='w-4/5 text-center font-light text-xl lg:text-base'>Multiples museos de diferentes partes del mundo son partes de nuestra red.</h4>
+      <div className='w-full h-1/2 md:w-1/2 md:h-full flex justify-center items-center flex-col gap-2 order-1 md:order-2 '>
+        <h2 className='w-full font-extrabold text-center text-xl'>¿Quienes participan?</h2>
+        <h4 className='w-full text-center font-light text-sm lg:text-base'>Multiples museos de diferentes partes del mundo son partes de nuestra red.</h4>
       </div>
     </section>
   )
