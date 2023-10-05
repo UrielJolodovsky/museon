ac5841fc83b2c2e29f171b6ef0fe681d03cc4277
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2023-10-05 11:02:54 -0300

    minor changes

--- prueba-proyecto/src/app/inicio/components/start/Participants.tsx
+++ prueba-proyecto/src/app/inicio/components/start/Participants.tsx
@@ -5,17 +5,10 @@ import { CldImage } from 'next-cloudinary'
 
 const Participants = () => {
   return (
-<<<<<<< HEAD
     <section className='w-[300px] h-[400px] flex justify-center items-center flex-row gap-1 container-participants '>
       <div className=' h-full lg:h-1/2 lg:w-full flex justify-center items-center'>
         <div className='w-full h-1/2 flex justify-end items-end md:order-2 md:flex md:justify-center md:items-center'>
           <CldImage src='PiramidIcon' width={0} height={0} alt={"Piramid"} className='w-[600px] h-[500px]' />
-=======
-    <section className='w-2/3 h-[400px] flex justify-center items-center flex-row gap-1 container-participants '>
-      <div className='w-1/2 h-full lg:h-1/2 lg:w-full flex justify-center items-center'>
-        <div className='w-full h-full flex justify-center items-center md:order-2 md:flex md:justify-center md:items-center'>
-          <CldImage src='PiramidIcon' alt='PiramidImage' width={0} height={0} className='piramid-image' ></CldImage>
->>>>>>> 74c709726c883405401950c1f3afcfa5b27825f9
         </div>
       </div>
       <div className='w-1/2 flex justify-center items-center flex-col gap-2 part-texts '>
