cf2537d6c32810cfd4232bd9081c87efc8e907d0
tomaslami <tomaslamiguralnik@gmail.com>
2023-11-08 15:00:00 -0300

    new

--- prueba-proyecto/src/app/colecciones/page.tsx
+++ prueba-proyecto/src/app/colecciones/page.tsx
@@ -11,82 +11,20 @@ import useMuseos from '@/hooks/useMuseos';
 
 const Colecciones = () => {
   const [museos, setMuseos] = useState<MuseosProps[]>([])
-  const [tipoMuseo, setTipoMuseo] = useState('')
   const [nomMuseo, setNomMuseo] = useState('')
   const router = useRouter()
-  const filtro = ['Deporte', 'Arte', 'Historia', 'Galerias']
-  const [filtered, setFiltered] = useState<MuseosProps[]>([]);
-
-
-  const FilterMuseums = (search: String) => {
-    // if (search === '' || type === '') {
-    //   setFiltered(museos)
-    // }
-    // // Filtrar cuando search y type no estén vacios
-    // if (search !== '') {
-    //   setFiltered(filtered.filter((museo) => {
-    //     return museo.name.toLowerCase().includes(search.toLowerCase())
-    //   }))
-    // }
-    // if (type !== '') {
-    //   setFiltered(filtered.filter((museo) => {
-    //     return museo.role.toLowerCase() === type.toLowerCase()
-    //   }))
-    // }
-    if (search === '') setFiltered(museos)
-    // else if (search === '') setFiltered(museos.filter((museo) => museo.role.toLowerCase() === type.toLowerCase()))
-    // else if (type === '') setFiltered(museos.filter((museo) => museo.name.toLowerCase().includes(search.toLowerCase())))
-    else {
-      setFiltered(museos.filter((museo) => {
-        return museo.name.toLowerCase().includes(search.toLowerCase())
-      }
-      ))
-    }
-  }
 
 
   useEffect(() => {
     useMuseos().then((res) => {
       setMuseos(res)
-      setFiltered(res)
     })
   }, [])
-
-
-  // const viewMuseos = async () => {
-  //   await axios.get(`${dir_url}/api/museos`)
-  //     .then((response) => {
-  //       setMuseos(response.data)
-  //       setFiltered(response.data)
-  //     })
-  // }
-
-
-  const changeSelect = (ev: any) => {
-    setTipoMuseo(ev.target.value)
-  }
-
-
-  const changeSearch = (ev: any) => {
-    FilterMuseums(ev.target.value)
-    setNomMuseo(ev.target.value)
-    console.log(nomMuseo)
-  }
-
-
-
-
-
 
   const deleteSpace = (slug: string) => {
     const newSlug = slug.replace(/\s/g, '-')
     return newSlug
   }
-
-
-
-
-
 
   return (
     <div className='w-full h-screen pt-20 md:px-40 flex flex-col justify-center items-center  '>
