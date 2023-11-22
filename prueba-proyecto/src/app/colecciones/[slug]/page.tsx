a22aced9bca26dc16eb7d7570b203b95d0087f79
tomaslami <tomaslamiguralnik@gmail.com>
2023-11-21 23:20:01 -0300

    comment fixed

--- prueba-proyecto/src/app/colecciones/[slug]/page.tsx
+++ prueba-proyecto/src/app/colecciones/[slug]/page.tsx
@@ -166,33 +166,27 @@ export default function Museo() {
                             </form>
                             <div className='w-full  flex justify-center items-start flex-col gap-4 '>
                                 {sessionData?.user ? (
-                                    messages.map((museo, index) =>
-                                        <div id='container-addComments' className=' w-full h-auto flex justify-center items-start flex-col gap-2 p-10 rounded-lg' key={index}>
+                                    Array.isArray(messages) ? messages.map((museo, index) =>
+                                        <div className=' w-full h-auto flex justify-center items-start flex-col gap-2 p-10 rounded-lg' key={index}>
                                             <h2 className='text-center font-bold text-black'>@{museo["author"]["name"]}</h2>
                                             <h1 className='text-center text-black'>{museo["content"]}</h1>
                                             <div className='flex w-full h-10 justify-start items-center flex-row gap-5'>
                                                 <button
                                                     id={index.toString()}
                                                     onClick={(event: MouseEvent<HTMLButtonElement>) => { AddDeleteLike(event, museo['id']) }}
-                                                    className={cn({
-                                                        'fill-red': liked.find((like) => like.commentId === museo['id'])
-                                                    })}
                                                 >
                                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="contentColor" stroke='black' className="w-6 h-6 stroke-2">
                                                         <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
                                                     </svg>
                                                 </button>
-                                                {/* <button id='addComment' onClick={() => openModal(index.toString())} className=''>
-                                                    <h2 className='font-bold'>Responder</h2>
-                                                </button> */}
-                                                {modalStates[index.toString()] && ReactDOM.createPortal(
-                                                    <ModalComentario isOpen={modalStates[index.toString()]} onClose={() => closeModal(index.toString())} />,
-                                                    document.getElementById('container-addComments') as HTMLElement
-                                                )}
+                                                {/* <button id='addComment' className=''>
+                                                <h2 className='font-bold'>Responder</h2>
+                                            </button> */}
                                             </div>
                                         </div>
-                                    )) : (
-                                    messages.map((museo, index) =>
+
+                                    ) : '') : (
+                                    Array.isArray(messages) ? messages.map((museo, index) =>
                                         <div className=' w-full h-auto flex justify-center items-start flex-col gap-2 p-10 rounded-lg' key={index}>
                                             <h2 className='text-center font-bold text-black'>@{museo["author"]["name"]}</h2>
                                             <h1 className='text-center text-black'>{museo["content"]}</h1>
@@ -210,8 +204,8 @@ export default function Museo() {
                                                 </button> */}
                                             </div>
                                         </div>
-                                    )
-                                )}
+
+                                    ) : '')}
                             </div>
                         </div>
                     </div>
